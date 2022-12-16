9d2e2f17fc0370429ded7bb84c96c5705892d403
SeminNikolay <Nik.urist@mail.ru>
2022-12-16 15:23:42 +0300

    Work_06 готов

---
+++ Seimnar_1/Work_06/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.Write("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = a/10%10;
+Console.Write("второе число у вас: ");
+Console.WriteLine(b);
