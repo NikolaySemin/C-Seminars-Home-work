be012cd9604ceb571e98e7595ed74d043a12a432
SeminNikolay <Nik.urist@mail.ru>
2022-12-16 15:37:32 +0300

    Work_08 готов

---
+++ Seimnar_1/Work_08/Program.cs
@@ -0,0 +1,6 @@
+﻿// Удалить вторую цифру трёхзначного числа.
+Console.WriteLine("Введите трехзначное число");
+int a = int.Parse(Console.ReadLine()!);
+int b = a / 100 * 10 + a % 10;
+Console.Write("Без второго числа выглядит так: ");
+Console.WriteLine(b);
