e879829b16500bab7366bf2b6bde442215d2f647
SeminNikolay <Nik.urist@mail.ru>
2022-12-16 23:38:32 +0300

    Work01 второй вариант решения готов

--- Seminar_2/Work01/Program.cs
+++ Seminar_2/Work01/Program.cs
@@ -1,53 +1,64 @@
 ﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// Console.Write("Введите число от 1 до 7: ");
+// int num = int.Parse(Console.ReadLine()!);
+
+// int a = 1;
+// int b = 2;
+// int c = 3;
+// int d = 4;
+// int e = 5;
+// int f = 6;
+// int g = 7;
+
+// if(num == a)
+// {
+//     Console.WriteLine("Понедельник");
+//     Console.Write("не выходной день");
+// }
+// if(num == b)
+// {
+//     Console.WriteLine("Вторник");
+//     Console.Write("не выходной день");
+
+// }
+// if(num == c)
+// {
+//     Console.WriteLine("Среда");
+//     Console.Write("не выходной день");
+
+// }
+// if(num == d)
+// {
+//     Console.WriteLine("Четверг");
+//     Console.Write("не выходной день");
+
+// }
+// if(num == e)
+// {
+//     Console.WriteLine("Пятница");
+//     Console.Write("не выходной день");
+
+// }
+// if(num == f)
+// {
+//     Console.WriteLine("Суббота");
+//     Console.Write("выходной день");
+
+// }
+// if(num == g)
+// {
+//     Console.WriteLine("Воскресенье");
+//     Console.Write("выходной день");
+
+// }
 Console.Write("Введите число от 1 до 7: ");
 int num = int.Parse(Console.ReadLine()!);
 
-int a = 1;
-int b = 2;
-int c = 3;
-int d = 4;
-int e = 5;
-int f = 6;
-int g = 7;
-
-if(num == a)
+if (num <= 5 )
 {
-    Console.WriteLine("Понедельник");
-    Console.Write("не выходной день");
+    Console.WriteLine("будни");
 }
-if(num == b)
+else
 {
-    Console.WriteLine("Вторник");
-    Console.Write("не выходной день");
-
-}
-if(num == c)
-{
-    Console.WriteLine("Среда");
-    Console.Write("не выходной день");
-
-}
-if(num == d)
-{
-    Console.WriteLine("Четверг");
-    Console.Write("не выходной день");
-
-}
-if(num == e)
-{
-    Console.WriteLine("Пятница");
-    Console.Write("не выходной день");
-
-}
-if(num == f)
-{
-    Console.WriteLine("Суббота");
-    Console.Write("выходной день");
-
-}
-if(num == g)
-{
-    Console.WriteLine("Воскресенье");
-    Console.Write("выходной день");
-
+    Console.WriteLine("выходной");
 }
