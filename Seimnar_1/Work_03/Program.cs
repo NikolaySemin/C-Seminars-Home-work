607d3402efef97c165d3647982cb18a0c93e3246
SeminNikolay <Nik.urist@mail.ru>
2022-12-16 12:06:55 +0300

    Work_03 готов

---
+++ Seimnar_1/Work_03/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти максимальное из трех чисел
+int a = 5;
+int b = 3;
+int c = 6;
+int max = a;
+if (a > max) max =a;
+if (b > max) max =b;
+if (c > max) max =c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
