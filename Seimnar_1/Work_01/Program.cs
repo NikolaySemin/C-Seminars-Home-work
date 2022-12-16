67d6a232e5c85ca235c9f9a9562119853a469a3c
SeminNikolay <Nik.urist@mail.ru>
2022-12-16 10:51:39 +0300

    Work_1 готов

--- Seimnar_1/Work_01/Program.cs
+++ Seimnar_1/Work_01/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число: ");
+int b = int.Parse(Console.ReadLine()!);
+if(b == a*a)
+{
+    Console.Write("число является квадратом: ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("число не является квадратом: ");
+    Console.WriteLine(a);
+}
