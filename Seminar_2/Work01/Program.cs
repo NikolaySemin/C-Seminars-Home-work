802702a4f68d129d65defd04323c747c9bad1a85
SeminNikolay <Nik.urist@mail.ru>
2022-12-18 12:46:21 +0300

    Work01 готов

---
+++ Seminar_2/Work01/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = a/10%10;
+Console.Write("второе число у вас: ");
+Console.WriteLine(b);
+
+
+// Console.WriteLine("Введите трехзначное число.\n Для выхода из программы введите q.");
+//     while(true)
+//     {
+//         string input = Console.ReadLine()!;
+//         if(!input.Equals("q")!)
+//             Console.WriteLine("{0}->{1}",input, input[1]);
+//         else
+//             break;
+//     }
