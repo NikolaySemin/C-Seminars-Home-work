86c449bcc1534852a7d781105aae494de736df82
SeminNikolay <Nik.urist@mail.ru>
2022-12-19 23:35:13 +0300

    Work19 мучаю задачу

--- Seminar_3/Work_19/Program.cs
+++ Seminar_3/Work_19/Program.cs
@@ -1,3 +1,43 @@
 ﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 // (читается слева направо и справа налево одинаково)
 
+// Console.Write("Введите пятизначное число: ");
+// int a = int.Parse(Console.ReadLine()!);
+
+// for(a.ToString[0], a =[-1]; a<a.Length;a++,a.Length--)
+// {
+//     if(a!=aLength)
+//     {
+//         Console.WriteLine("Не является палиндромом");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Является палиндромом");  
+//     }
+// }
+
+
+
+//     class Palindr
+//      {
+//         public static bool Palindromtest (string s)
+//         {
+//                for (int i = 0, j = s.Length - 1; i < j; i++, j--)
+ 
+//                     if (s[i] != s[j])
+//                         return false;
+//                        return true;
+ 
+//         }
+     
+//     static void Main()
+//         {
+//         string s;
+//             Console.WriteLine("Анализ палиндромов\n\nВведите строку:");
+//           s = Console.ReadLine();
+//            if (Palindromtest(s))
+//        Console.WriteLine("Эта строка - палиндром");
+//        else Console.WriteLine("Эта строка - не палиндром");
+//     Console.ReadLine();
+//         }
+// }
