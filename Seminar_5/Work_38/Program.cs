cd5274e48543d0ff07e9140a9132070b78307308
SeminNikolay <Nik.urist@mail.ru>
2022-12-23 16:45:05 +0300

    work_38 done

---
+++ Seminar_5/Work_38/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+void FillArrayRandom(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+        if (i == array.Length - 1)
+        {
+            Console.Write(array[i] + "]");
+        }
+        else
+        {
+            Console.Write(array[i] + ", ");
+        }
+    }
+}
+
+int[] array = new int[5];
+FillArrayRandom(array);
+Console.WriteLine("массив: ");
+PrintArray(array);
+int min = Int32.MaxValue;
+int max = Int32.MinValue;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+Console.WriteLine($" => {max - min}");
+
