7b2885673aba1b4fe0988aee20ba18f3c3cebf6b
SeminNikolay <Nik.urist@mail.ru>
2022-12-24 11:35:05 +0300

    work_41 done

---
+++ Seminar_6/Work_41/Program.cs
@@ -0,0 +1,38 @@
+﻿// // Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Пример:
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа через пробел: ");
+string inputText = Console.ReadLine(); // "4 5 2"
+
+string[] stringArray = inputText.Split(' '); // ["4", "5", "2"]
+
+int[] array = new int[stringArray.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = int.Parse(stringArray[i]);
+}
+Console.Write("[ ");
+for (int i = 0; i < array.Length; i++)
+{
+    if (i == array.Length - 1)
+    {
+        Console.Write(array[i] + "]");
+    }
+    else
+    {
+        Console.Write(array[i] + ", ");
+    }
+
+}
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]>0)
+    {
+        count++;
+    }
+}
+Console.Write(" -> "+count);
