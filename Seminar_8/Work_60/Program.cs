3ec404a6e7bd9b21bc5cc8d30e22dfaff9200b1b
SeminNikolay <Nik.urist@mail.ru>
2022-12-28 11:33:09 +0300

    work_60 done

---
+++ Seminar_8/Work_60/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+void FillMatrix(int[,,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr.GetLength(2); k++)
+            {
+                matr[i, j, k] = new Random().Next(10, 100);
+            }
+
+        }
+    }
+}
+
+void PrintMatrix(int[,,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr.GetLength(2); k++)
+            {
+                Console.Write(matr[i, j, k] + " ");
+                Console.Write($"{(i, j, k)} ");
+            }
+            Console.WriteLine();
+
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,,] matrix = new int[2, 2, 2];
+FillMatrix(matrix);
+PrintMatrix(matrix);
