9e83c1f3040d8f9bf9e95a149940c91aeb9bd60d
SeminNikolay <Nik.urist@mail.ru>
2022-12-28 13:52:01 +0300

    work_54 done

---
+++ Seminar_8/Work_54/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void FillMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SelectionSort(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+Console.Clear();
+Console.Write("Введите количетво столбцов прямоугольного массива: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количетво строк прямоугольного массива: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+
+int[,] matrix = new int[m, n];
+Console.WriteLine("Задан массив:\n");
+FillMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+Console.WriteLine("В итоге получается вот такой отсортированный массив:\n");
+SelectionSort(matrix);
+PrintMatrix(matrix);
+
+
